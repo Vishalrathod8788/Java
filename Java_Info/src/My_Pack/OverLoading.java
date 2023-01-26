b62cfe06cd7f084afc509ae837fe16fe7ead8e64
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-26 18:29:31 +0530

    Add

---
+++ Java_Info/src/My_Pack/OverLoading.java
@@ -0,0 +1,16 @@
+package My_Pack;
+
+public class OverLoading 
+{
+
+	int a,b;
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	public double sum(double a, double b)
+	{
+		return a+b;
+	}
+
+}
---
+++ Java_Info/src/My_Pack/Test_Oveload.java
@@ -0,0 +1,11 @@
+package My_Pack;
+
+public class Test_Oveload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OverLoading over = new OverLoading();
+		System.out.println(over.sum(1.2, 1.3));
+	}
+
+}
