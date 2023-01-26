6957bdcc7ced2219ded56f838e09160680eea700
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-26 17:47:27 +0530

    Add

---
+++ Java_Info/src/My_Pack/For_Each.java
@@ -0,0 +1,16 @@
+package My_Pack;
+
+public class For_Each 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String[] arr = {"VGR", "Digu", "Abhi", "Sahil"};
+		for(String VGR : arr)
+		{
+			System.out.println(VGR + "");
+		}
+	}
+
+}
