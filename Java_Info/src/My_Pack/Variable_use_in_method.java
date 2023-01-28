0453400bf15a9f9fffa0be2c45d349f23b225ab1
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-28 20:38:10 +0530

    Add

--- Java_Info/src/My_Pack/Variable_use_in_method.java
+++ Java_Info/src/My_Pack/Variable_use_in_method.java
@@ -1,12 +1,16 @@
 package My_Pack;
 
+class A
+{
+	int x = 10;
+}
 public class Variable_use_in_method 
 {
-
+	
 	public static void main(String[] args) 
 	{
-	
-
+		A a1 = new A();
+		System.out.println(a1.x);
 	}
 
 }
