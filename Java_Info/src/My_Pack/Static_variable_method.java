520f5de43e5f7d4d8a95202ecbf38cac088ab8d4
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-29 20:24:40 +0530

    Add Static...

---
+++ Java_Info/src/My_Pack/Static_variable_method.java
@@ -0,0 +1,18 @@
+package My_Pack;
+
+public class Static_variable_method 
+{
+	static int x = 10;
+	static int y = 20;
+	
+	public Static_variable_method() 
+	{
+		
+	}
+
+	public static void main(String[] args) 
+	{
+	
+	}
+
+}
