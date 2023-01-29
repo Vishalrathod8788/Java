230fb15b81c9d45beeb4b987e6d64f4aa24ccb2e
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-29 20:41:36 +0530

    Add Static...

--- Java_Info/src/My_Pack/Static_variable_method.java
+++ Java_Info/src/My_Pack/Static_variable_method.java
@@ -4,10 +4,19 @@ public class Static_variable_method
 {
 	static int x = 10;
 	static int y = 20;
+	
+	public static int sum(int a, int b)
+	{
+		x = a;
+		y = b;
+		System.out.println("Sum Of " + (x + y));
+		return x+y;
+	}
 
 	public static void main(String[] args) 
 	{
-	
+		sum(30, 40);
+		
 	}
 
 }
