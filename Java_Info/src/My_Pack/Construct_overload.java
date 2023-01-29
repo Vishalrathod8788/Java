09492132c6948db6684a000cd19a1bb38e773204
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-29 20:11:02 +0530

    Add File

--- Java_Info/src/My_Pack/Construct_overload.java
+++ Java_Info/src/My_Pack/Construct_overload.java
@@ -2,9 +2,12 @@ package My_Pack;
 
 public class Construct_overload 
 {
-	public Construct_overload()
+	int x, y;
+	public Construct_overload(int a, int b)
 	{
-		
+		this.x = a;
+		this.y = b;
+		System.out.println("Sum Of Two Numbers "+ a+b);
 	}
 	public static void main(String[] args) 
 	{
