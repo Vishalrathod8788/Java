9fcb2d6b7265d1131921eff34f0d509013d0c241
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-30 17:55:18 +0530

    Add File

--- Java_Info/src/My_Pack/MultiLeval.java
+++ Java_Info/src/My_Pack/MultiLeval.java
@@ -1,10 +1,43 @@
 package My_Pack;
 
-public class MultiLeval {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MultiLeval 
+{
+	Scanner sc = new Scanner(System.in);
+	class person 
+	{
+		String name, bdate;
+		boolean gender;
+		public person()
+		{
+			System.out.println("This Is Person Class");	
+		}
 	}
-
+	
+	class StudentDetail extends person
+	{
+		String course, collage, university;
+		public StudentDetail()
+		{
+			System.out.println("This Is Univercity Class");
+		}
+	}
+	
+	class Mark extends StudentDetail
+	{
+		char Grade;
+		int sem, m1, m2, m3;
+		float avrg;
+		
+		void Display()
+		{
+			
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
 }
