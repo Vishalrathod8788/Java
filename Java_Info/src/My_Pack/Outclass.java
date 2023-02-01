e2a8afbae3cbf640828ee71f8e2a0a4f886c51f2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-01 21:16:02 +0530

    Add

---
+++ Java_Info/src/My_Pack/Outclass.java
@@ -0,0 +1,16 @@
+package My_Pack;
+
+public class Outclass 
+{
+	int id;
+	String name;
+}
+class TestOut
+{
+	public static void main(String[] args) {
+		Outclass a = new Outclass();
+		System.out.println(a.id);
+		System.out.println(a.name);
+	}
+			
+}
