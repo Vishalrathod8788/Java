94876a8991ff1f2cba0ed494742c0f5faf5a6fe8
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-01 18:56:18 +0530

    Add

---
+++ Java_Info/src/My_Pack/Abstract.java
@@ -0,0 +1,30 @@
+package My_Pack;
+
+abstract class A12
+{
+	abstract void name();
+	public void show()
+	{
+		System.out.println("This is Abstract Method ");
+	}
+}
+
+public class Abstract extends A12
+{
+
+	@Override
+	void name() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("VGR");
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Abstract a = new Abstract();
+		a.show();
+		a.name();
+	}
+
+}
