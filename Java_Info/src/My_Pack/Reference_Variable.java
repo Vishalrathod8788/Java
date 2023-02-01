78e2a076ee08ce8173fd8a7224a4ded65458fdf5
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-01 21:26:29 +0530

    Add

---
+++ Java_Info/src/My_Pack/Reference_Variable.java
@@ -0,0 +1,19 @@
+package My_Pack;
+
+class B
+{
+	int id;
+	String name;
+}
+
+public class Reference_Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B a = new B();
+		a.id = 1;
+		a.name = "VGR";
+		System.out.println("Id : " + a.id + "\n" +"Name : " + a.name );
+	}
+
+}
