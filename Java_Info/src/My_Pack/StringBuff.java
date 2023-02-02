6d5c50a79c1c2d76a94bd21f793895f27ba7eb89
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-02 19:31:45 +0530

    Some Add

--- Java_Info/src/My_Pack/StringBuff.java
+++ Java_Info/src/My_Pack/StringBuff.java
@@ -4,10 +4,17 @@ public class StringBuff {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StringBuffer str = new StringBuffer("Hell");
+		String name = "VGR";
+		StringBuffer str = new StringBuffer("Hyyy");
 		System.out.println(str.charAt(3));
 		System.out.println(str.capacity());
 		System.out.println(str.length());
+		System.out.println(str.codePointAt(2));
+		System.out.println(str.codePointBefore(2));
+		System.out.println(str.codePointCount(0, str.length()));
+		System.out.println(str.isEmpty());
+		System.out.println(str.equals(name));
+		System.out.println(str.substring(3));
 	}
 
 }
