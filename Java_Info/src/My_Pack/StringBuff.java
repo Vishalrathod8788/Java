b8592227e114fc4dc2ba4f57bb3393b05c6d48a8
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-02 19:13:44 +0530

    String Buff

---
+++ Java_Info/src/My_Pack/StringBuff.java
@@ -0,0 +1,11 @@
+package My_Pack;
+
+public class StringBuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer str = new StringBuffer("Hell");
+		System.out.println(str.length());
+	}
+
+}
