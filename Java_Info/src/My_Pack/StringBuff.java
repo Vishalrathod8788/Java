c0d8647bfcac6f1ed67fcdf9d070d79fed5d1fb2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-02 19:15:29 +0530

    Method Add

--- Java_Info/src/My_Pack/StringBuff.java
+++ Java_Info/src/My_Pack/StringBuff.java
@@ -5,6 +5,8 @@ public class StringBuff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		StringBuffer str = new StringBuffer("Hell");
+		System.out.println(str.charAt(3));
+		System.out.println(str.capacity());
 		System.out.println(str.length());
 	}
 
