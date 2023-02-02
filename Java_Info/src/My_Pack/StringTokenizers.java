0fd7c1192d6b3d6074acf770d6845345c4265c64
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-02 19:58:08 +0530

    String Token

--- Java_Info/src/My_Pack/StringTokenizers.java
+++ Java_Info/src/My_Pack/StringTokenizers.java
@@ -1,9 +1,14 @@
 package My_Pack;
 
+import java.util.StringTokenizer;
+
 public class StringTokenizers {
 	public static void main(String[] args) {
-		String str = "vishal,rathod:vercel.app";
-
+//		String str = "vishal;rathod:vercel.app";
+		
+		String str = "vishal-rathod,vercel.app";
+		StringTokenizer tokens = new StringTokenizer(str,  "-,.");
+		while ( tokens.hasMoreTokens() )
+			System.out.println(tokens.nextToken());
 	}
-
 }
