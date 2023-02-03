091b69226de7801d55d476c19a6092389db0c823
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-03 16:00:10 +0530

    Some Change

--- Java_Info/src/My_Pack/toString.java
+++ Java_Info/src/My_Pack/toString.java
@@ -1,14 +1,16 @@
 package My_Pack;
 
+
 public class toString
 {
 	int no = 0;
 	String name = "VGR";
-	void name()
+	public toString(int no, String name)
 	{
-		System.out.println("No "+no + "\nName " + name);
+		this.no = no;
+		this.name = name;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "toString [no=" + no + ", name=" + name + "]";
@@ -16,9 +18,9 @@ public class toString
 
 	public static void main(String[] args) 
 	{
-			toString t1 = new toString();
-			t1.name();
-			t1.toString();
+			toString t1 = new toString(1, "VGR");
+			toString t2 = new toString(2, "Vishal");
+			System.out.println(t1.toString());
 	}
 	
 }
