075553722fded2d5d0846c0eff0059a947cb4b8d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-03 17:01:13 +0530

    Add Vactor

---
+++ Java_Info/src/My_Pack/VectorStud.java
@@ -0,0 +1,48 @@
+package My_Pack;
+
+public class VectorStud 
+{
+	private int no;
+	private String name;
+	
+	
+	public int getNo() {
+		return no;
+	}
+
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public VectorStud(int no, String name) {
+		super();
+		this.no = no;
+		this.name = name;
+	}
+
+
+	public static void main(String[] args) 
+	{
+
+		
+	}
+
+
+	@Override
+	public String toString() {
+		return "VectorStud [no=" + no + ", name=" + name + "]";
+	}
+
+}
--- Java_Info/src/My_Pack/VectorsDemo.java
+++ Java_Info/src/My_Pack/VectorsDemo.java
@@ -18,16 +18,22 @@ public class VectorsDemo {
 		v1.addElement(new Long(1212121));
 		v1.addElement(new Short((short)324));
 		
-		System.out.println(v1.get(0));
-		System.out.println(v1.get(1));
-		System.out.println(v1.get(2));
-		System.out.println(v1.get(3));
-		System.out.println(v1.get(4));
-		System.out.println(v1.get(5));
-		System.out.println(v1.get(6));
-		System.out.println(v1.get(7));
-//		System.out.println(v1.get(8));
-//		System.out.println(v1.get(9));
+		VectorStud p1 = new VectorStud(1, "VGR");
+		VectorStud p2 = new VectorStud(2, "Vishal");
+		v1.addElement(p1);
+		v1.addElement(p2);
+		
+		System.out.println("1"+v1.get(0));
+		System.out.println("2"+v1.get(1));
+		System.out.println("3"+v1.get(2));
+		System.out.println("4"+v1.get(3));
+		System.out.println("5"+v1.get(4));
+		System.out.println("6"+v1.get(5));
+		System.out.println("7"+v1.get(6));
+		System.out.println("8"+v1.get(7));
+		System.out.println("9"+v1.get(8));
+		System.out.println("10"+v1.get(9));
+//		System.out.println("11"+v1.get(10));
 	}
 
 }
