2f1eae91a5b17f5521f97cf5da21baf0da27d559
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-05 19:33:06 +0530

    Add

--- Java_Info/src/My_Pack/WrapperDemo.java
+++ Java_Info/src/My_Pack/WrapperDemo.java
@@ -7,7 +7,9 @@ public class WrapperDemo {
 	public static void main(String[] args) {
 		Vector vector = new Vector();
 		vector.addElement(new Integer(5));
+		vector.addElement(new Float(10.1111));
 		
 		System.out.println("1 : " + vector.get(0));
+		System.out.println("1 : " + vector.get(1));
 	}
 }
