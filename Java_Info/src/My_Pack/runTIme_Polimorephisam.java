bd9f24577a53f4987f7c2b6a5c1c3d4fb5eba005
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-06 21:45:36 +0530

    Add

--- Java_Info/src/My_Pack/runTIme_Polimorephisam.java
+++ Java_Info/src/My_Pack/runTIme_Polimorephisam.java
@@ -1,10 +1,24 @@
 package My_Pack;
 
-public class runTIme_Polimorephisam {
+abstract class Bike
+{
+	public abstract void run();
 
-	public static void main(String[] args) {
+}
+
+public class runTIme_Polimorephisam extends Bike
+{
+
+	
+	public static void main(String[] args) 
+	{
+
+	}
+
+	@Override
+	public void run() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Runn................");
 	}
 
 }
