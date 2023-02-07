d5b88cbe42336681a7b98069b4c8fd31c4686f28
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-07 22:00:40 +0530

    Add

--- Java_Info/src/My_Pack/Sum.java
+++ Java_Info/src/My_Pack/Sum.java
@@ -2,10 +2,17 @@ package My_Pack;
 
 public class Sum 
 {
-	
+	int a, b;
+	public void Sum(int a, int b)
+	{
+		this.a = a;
+		this.b = b;
+		System.out.println("Sum Of A and B :"+ (a+b));
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Sum a = new Sum();
+		a.Sum(2, 3);
 	}
 
 }
