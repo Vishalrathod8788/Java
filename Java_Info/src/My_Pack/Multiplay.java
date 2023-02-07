1b0c78376f59dcb1b288d5a5c8e4852535aa34e2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-07 22:12:56 +0530

    Add

--- Java_Info/src/My_Pack/Multiplay.java
+++ Java_Info/src/My_Pack/Multiplay.java
@@ -2,11 +2,21 @@ package My_Pack;
 
 public class Multiplay
 {
-	int a , b;
+	//int a , b;
+	
+	public void Multiplay(int a, int b)
+	{
+		Sum obj = new Sum();
+		obj.a = a;
+		obj.b = b;
+		System.out.println("Multiplay Of 2 Numbers " + (a*b));
+	}
+	
 	
 	public static void main(String[] args)
 	{
-		
+		Multiplay m = new Multiplay();
+		m.Multiplay(2, 2);
 	}
 
 }
