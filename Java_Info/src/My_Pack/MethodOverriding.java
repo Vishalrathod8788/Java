7c514e80db1effbd1d861968ad4ebe9ec031e44c
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-07 22:55:43 +0530

    Add

--- Java_Info/src/My_Pack/MethodOverriding.java
+++ Java_Info/src/My_Pack/MethodOverriding.java
@@ -2,10 +2,22 @@ package My_Pack;
 
 public class MethodOverriding
 {
+	int a;
+	double c;
+	public void Overriding(int a, double c)
+	{
+		System.out.println("Method 1 : " + (a+c));
+	}
+	public void Overriding(double c, int a)
+	{
+		System.out.println("Method 2 : " + (a+c));
+	}
 	
 	public static void main(String[] args) 
 	{
-		
+		MethodOverriding m = new MethodOverriding();
+		m.Overriding(2, 2.2222);
+		m.Overriding(2.2222, 2);
 		
 	}
 }
