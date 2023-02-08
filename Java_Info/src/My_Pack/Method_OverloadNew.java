8a7829512acf28513c21cdcd42a6055fb77f666a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-08 23:45:57 +0530

    Method Create

--- Java_Info/src/My_Pack/Method_OverloadNew.java
+++ Java_Info/src/My_Pack/Method_OverloadNew.java
@@ -2,6 +2,15 @@ package My_Pack;
 
 public class Method_OverloadNew {
 	
+	int a;
+	double b;
+	void Method(int x, double y)
+	{
+		this.a = x;
+		this.b = y;
+		System.out.println("Sum Of Two Numbers : " + (a+b));
+	}
+
 	
 
 	public static void main(String[] args) {
