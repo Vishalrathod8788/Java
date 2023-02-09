4aa79d2f324cfbc30bcca2de69c36aa094a89c5e
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-09 22:45:17 +0530

    ++

--- Java_Info/src/My_Pack/Thread1.java
+++ Java_Info/src/My_Pack/Thread1.java
@@ -1,6 +1,11 @@
 package My_Pack;
 
 public class Thread1 {
+	
+	public void run()
+	{
+		System.out.println("Hello...");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
