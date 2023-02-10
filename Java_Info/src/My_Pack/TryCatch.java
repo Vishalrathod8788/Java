7ad5efdf079217e8b4f41ee08cb59704630e07a2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-10 23:30:11 +0530

    Try Catch

--- Java_Info/src/My_Pack/TryCatch.java
+++ Java_Info/src/My_Pack/TryCatch.java
@@ -1,19 +1,20 @@
 package My_Pack;
 
-public class TryCatch 
+public class TryCatch  extends Thread
 {
 	public static void main(String[] args) 
 	{
 		try 
 		{
-			for(int x=0; x<=4; x++)
+			int a[] = new int[5];
+			for(int x=0; x<10; x++)
 			{
-				
+				System.out.println(a[x]);
 			}
 		} 
 		catch (Exception e)
 		{
-
+			System.out.println("Error....");
 		}
 	}
 }
