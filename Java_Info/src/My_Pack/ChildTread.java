f21c554773c688aa1970a543d8d2ce2bbbe63322
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-13 19:08:00 +0530

    Add..

--- Java_Info/src/My_Pack/ChildTread.java
+++ Java_Info/src/My_Pack/ChildTread.java
@@ -6,7 +6,13 @@ public class ChildTread extends Thread
 	public static void main(String[] args) 
 	{
 		Thread t = Thread.currentThread();
-		System.out.println("Current Thread :- "+ t.getName());
+		System.out.println("Current Thread :- " + t.getName());
+		
+		t.setName("Vishal");
+		System.out.println("After Change Name " + t.getName());
+		
+		System.out.println("Thread Prority " + t.getPriority());
+		
 	}
 
 }
