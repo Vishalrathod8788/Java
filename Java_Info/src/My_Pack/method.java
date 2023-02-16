8766decf8687db0147d32e8a0b3451a122847992
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-16 23:10:27 +0530

    ++

--- Java_Info/src/My_Pack/method.java
+++ Java_Info/src/My_Pack/method.java
@@ -1,11 +1,13 @@
 package My_Pack;
 
-//import java.util.*;
-
 public class method
 {
-	public static void main(String[] args) {
+
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello...!");
+	
 	}
 }
 
