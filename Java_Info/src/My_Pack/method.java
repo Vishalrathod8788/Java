c64876f9b66b99dfb220745d25836ec0779c5d25
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-16 23:14:24 +0530

    ++

--- Java_Info/src/My_Pack/method.java
+++ Java_Info/src/My_Pack/method.java
@@ -3,11 +3,14 @@ package My_Pack;
 public class method
 {
 
-	
+	public void demo()
+	{
+		System.out.println("This is Method...");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello...!");
-	
+		
 	}
 }
 
