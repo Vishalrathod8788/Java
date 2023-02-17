2bd5b0e622d62f0600c22a1faa9a5d13544323d9
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-17 23:03:16 +0530

    Add

--- Java_Info/src/My_Pack/method.java
+++ Java_Info/src/My_Pack/method.java
@@ -3,10 +3,7 @@ package My_Pack;
 public class method
 {
 
-	public void demo()
-	{
-		System.out.println("This is Method...");
-	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello...!");
