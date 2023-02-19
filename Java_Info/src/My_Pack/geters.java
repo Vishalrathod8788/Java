d4b4ec8d1440ee27749f9c689c71fa3644a01f64
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-02-19 18:08:04 +0530

    Add

--- Java_Info/src/My_Pack/geters.java
+++ Java_Info/src/My_Pack/geters.java
@@ -3,6 +3,15 @@ package My_Pack;
 public class geters
 {
 
+	private String name;
+	private int age;
+	
+	public geters(String name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+
 	public static void main(String[] args) 
 	{
 		
