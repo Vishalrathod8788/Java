75907c5d8b69c7e9e9c7ca9e43782f4fa9a6c9a4
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-11 21:15:10 +0530

    Write in File

--- Java_Info/src/My_Pack/FileInputOutputDemo.java
+++ Java_Info/src/My_Pack/FileInputOutputDemo.java
@@ -9,7 +9,15 @@ public class FileInputOutputDemo
 
 	public static void main(String[] args) throws Exception 
 	{
+		FileOutputStream fos = new FileOutputStream("/home/vishal/Desktop/fos.txt", true);
 		
+		String Data = "Hello....!";
+		
+		fos.write(Data.getBytes());
+		
+		fos.close();
+		
+		System.out.println("Data has been written to File 'fos.txt'");
 	}
 
 }
