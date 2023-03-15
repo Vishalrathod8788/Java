22a69acf483e565d6b1a49b7f88ab99e5631011d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-15 21:19:52 +0530

    Add...

--- Java_Info/src/My_Pack/mainThread.java
+++ Java_Info/src/My_Pack/mainThread.java
@@ -4,9 +4,12 @@ public class mainThread {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("This is Main Thread ");
+		System.out.println("This is Main Thread Demo");
 		Thread t = new Thread();
-		t.currentThread();
+		System.out.println(t.currentThread());
+		
+		t.setName("main_root_thread");
+		System.out.println();
 	}
 
 }
