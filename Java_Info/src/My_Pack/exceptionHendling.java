7b1b97453fa948f487798bdead581420ac4efdb5
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-19 09:54:59 +0530

    Arith_Excep...

--- Java_Info/src/My_Pack/exceptionHendling.java
+++ Java_Info/src/My_Pack/exceptionHendling.java
@@ -6,17 +6,20 @@ public class exceptionHendling {
 	{
 		int[] ex = new int[5];
 		System.out.println("Program Start...");
-		int i = 10/0;
 		
 		try
 		{
-			
+			int i = 10/0;
 			ex[7] = 10;
 		} 
 		catch (ArrayIndexOutOfBoundsException er) 
 		{
 			er.printStackTrace();	// This is Programing Error...
 			System.out.println("Array Index Out of Bound Exception");
+		}
+		catch (ArithmeticException e) 
+		{
+			System.out.println("Arithmetic Exception Error");
 		}
 		System.out.println("Program End...");
 	}
