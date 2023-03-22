525195670af3dfd929aa21c5e8cd31857f591f9d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-22 10:29:15 +0530

    Adddd

--- Java_Info/src/My_Pack/LifeCycleThread.java
+++ Java_Info/src/My_Pack/LifeCycleThread.java
@@ -1,10 +1,9 @@
 package My_Pack;
 
 
-public class LifeCycleThread implements Runnable 
+public class LifeCycleThread 
 {
 	
-	Thread t1 = new Thread();
 
 	public static void main(String[] args) 
 	{
