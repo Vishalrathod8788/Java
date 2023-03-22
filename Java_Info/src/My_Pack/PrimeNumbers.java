cd2bec2e711b0bee726b3713f62f2d9ad5e08537
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-22 18:44:44 +0530

    Add...

--- Java_Info/src/My_Pack/PrimeNumbers.java
+++ Java_Info/src/My_Pack/PrimeNumbers.java
@@ -3,17 +3,29 @@ package My_Pack;
 public class PrimeNumbers 
 {
 	
-	public static void main(String[] args) {
-        int n = 10; // Replace with the desired number of prime numbers
-        
-        System.out.println("The first " + n + " prime numbers are:");
-        int count = 0, i = 2;
-        while(count < n) {
-            if(isPrime(i)) {
-                System.out.print(i + " ");
-                count++;
-            }
-            i++;
-        }
-    }
+		public static void main(String[] args) {
+	        int n = 10; // Replace with the desired number of prime numbers
+	        
+	        System.out.println("The first " + n + " prime numbers are:");
+	        int count = 0, i = 2;
+	        while(count < n) {
+	            if(isPrime(i)) {
+	                System.out.print(i + " ");
+	                count++;
+	            }
+	            i++;
+	        }
+	    }
+		
+		  public static boolean isPrime(int num) {
+		        if(num < 2) {
+		            return false;
+		        }
+		        for(int i = 2; i <= Math.sqrt(num); i++) {
+		            if(num % i == 0) {
+		                return false;
+		            }
+		        }
+		        return true;
+		    }
 }
