505ef45348d04f92b186740e59efcd6307eab86a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-23 21:04:18 +0530

    Add...

--- Java_Info/src/My_Pack/MyFrame.java
+++ Java_Info/src/My_Pack/MyFrame.java
@@ -5,5 +5,43 @@ import java.awt.*;
 
 public class MyFrame extends JFrame
 {
-
+	public MyFrame() {
+        // Set the layout manager for the content pane
+        getContentPane().setLayout(new GridBagLayout());
+        
+        // Create a constraints object for the text field
+        GridBagConstraints textConstraints = new GridBagConstraints();
+        textConstraints.gridx = 0;
+        textConstraints.gridy = 0;
+        textConstraints.weightx = 1.0;
+        textConstraints.fill = GridBagConstraints.HORIZONTAL;
+        
+        // Create the text field and add it to the content pane
+        JTextField textField = new JTextField();
+        getContentPane().add(textField, textConstraints);
+        
+        // Create a constraints object for the label
+        GridBagConstraints labelConstraints = new GridBagConstraints();
+        labelConstraints.gridx = 0;
+        labelConstraints.gridy = 1;
+        labelConstraints.anchor = GridBagConstraints.WEST;
+        
+        // Create the label and add it to the content pane
+        JLabel label = new JLabel("Enter your name:");
+        getContentPane().add(label, labelConstraints);
+        
+        // Create a constraints object for the button
+        GridBagConstraints buttonConstraints = new GridBagConstraints();
+        buttonConstraints.gridx = 0;
+        buttonConstraints.gridy = 2;
+        buttonConstraints.anchor = GridBagConstraints.CENTER;
+        
+        // Create the button and add it to the content pane
+        JButton button = new JButton("Submit");
+        getContentPane().add(button, buttonConstraints);
+        
+        // Set the size of the frame and make it visible
+        setSize(300, 200);
+        setVisible(true);
+    }
 }
