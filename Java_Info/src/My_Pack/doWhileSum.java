0f3180927bdb18b7e264c5ef7993129320893ac7
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-25 23:46:42 +0530

    Add...

--- Java_Info/src/My_Pack/doWhileSum.java
+++ Java_Info/src/My_Pack/doWhileSum.java
@@ -1,5 +1,16 @@
 package My_Pack;
 
-public class doWhileSum {
-
+public class doWhileSum 
+{
+	public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        
+        do {
+            sum += i;
+            i++;
+        } while (i <= 10);
+        
+        System.out.println("The sum of numbers 1 to 10 is: " + sum);
+    }
 }
