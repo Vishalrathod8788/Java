8358114ff58c1c1502f78bd8d2614882e756c358
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-30 21:27:46 +0530

    Add

--- Java_Info/src/My_Pack/Test_Oveload.java
+++ Java_Info/src/My_Pack/Test_Oveload.java
@@ -5,7 +5,7 @@ public class Test_Oveload {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		OverLoading over = new OverLoading();
-		System.out.println(over.plush(2.2222, 2.2222));
+		System.out.println(over.plush(2.2222, 4.4444));
 	}
 
 }
