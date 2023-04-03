34f975d830f32435be87f3aff6d45265bc8f7ae1
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-03 12:20:03 +0530

    Add

--- Java_Info/src/My_Pack/Vector_Class.java
+++ Java_Info/src/My_Pack/Vector_Class.java
@@ -1,8 +1,24 @@
 package My_Pack;
 
+import java.util.Vector;
+
 public class Vector_Class {
 
 	public static void main(String[] args)
 	{
+		Vector v1 = new Vector();
+		
+		v1.addElement(new Integer(222));
+		
+		v1.insertElementAt("Hello Friends", 1);
+		v1.removeElement("Hello Friends");
+		v1.clear();
+		System.out.println("Vector Size : " + v1.size());
+		
+		for(int i=0; i<v1.size(); i++)
+		{
+			System.out.println(i +" : "+v1.get(i));	 
+		}
+		
 	}
 }
