25bb69cb388d497670f497933d17e7a5b8b06057
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-03 12:38:30 +0530

    Add

--- Java_Info/src/My_Pack/Str_Buff_Class.java
+++ Java_Info/src/My_Pack/Str_Buff_Class.java
@@ -4,7 +4,14 @@ public class Str_Buff_Class {
 
 	public static void main(String[] args) 
 	{
-		StringBuffer str = new StringBuffer();
+		StringBuffer str = new StringBuffer("Vishal");
+		String name = "VGR";
+		System.out.println(str.charAt(3));
+		System.out.println(str.insert(6, " Rathod"));
+		System.out.println(str.replace(0, 6, "VGR"));
+		System.out.println(str.isEmpty());
+		System.out.println(str.equals(name));
+		System.out.println(str.reverse());
 	}
 
 }
