d28e180ab7bf1967e590078e9bf5c91b63b42367
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-06 11:17:15 +0530

    Add...

--- Java_Info/src/My_Pack/MultiDArray.java
+++ Java_Info/src/My_Pack/MultiDArray.java
@@ -4,7 +4,12 @@ public class MultiDArray {
 
 	public static void main(String[] args) 
 	{
-
+		int number[][] = {{10,20,30,40,50},{60,70,80,90,100}};
+		
+		for(int i=0; i<=number.length; i++)
+		{
+			
+		}
 	}
 
 }
