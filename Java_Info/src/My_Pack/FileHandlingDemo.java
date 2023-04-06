acfc9260f8fdd2858512444663020731a7a60401
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-06 14:20:33 +0530

    Add...

--- Java_Info/src/My_Pack/FileHandlingDemo.java
+++ Java_Info/src/My_Pack/FileHandlingDemo.java
@@ -1,10 +1,14 @@
 package My_Pack;
 
+import java.io.*;
+
 public class FileHandlingDemo {
 
 	public static void main(String[] args) 
 	{
-
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
 	}
 
 }
