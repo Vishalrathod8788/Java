ba5cbf83fd27b13bbfe9b549bf8067384e90c23d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-06 11:03:59 +0530

    Add...

--- Java_Info/src/My_Pack/OneDArray.java
+++ Java_Info/src/My_Pack/OneDArray.java
@@ -1,13 +1,24 @@
 package My_Pack;
 
-public class OneDArray {
-
+public class OneDArray extends Thread
+{
+	
 	public static void main(String[] args) 
 	{
+		OneDArray t1 = new OneDArray();
 		int num[] = {10,20,30,40,50};
-		for(int i=0; i<=4; i++)
+		try
 		{
 			
+		}
+		catch (Exception e) 
+		{
+		
+		}
+		for(int i=0; i<=4; i++)
+		{
+			System.out.println(num[i]);
+			t1.sleep(i);
 		}
 	}
 
