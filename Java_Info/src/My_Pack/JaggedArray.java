46c1c9a7cd4d075432b53e710165fb5b8192241a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-07 12:58:58 +0530

    Add...

--- Java_Info/src/My_Pack/JaggedArray.java
+++ Java_Info/src/My_Pack/JaggedArray.java
@@ -9,19 +9,27 @@ public class JaggedArray
 		arr[1] = new int[4];
 		arr[2] = new int[2];
 		int count = 0;
-		for(int i=0; i<=arr.length; i++)
+		try
 		{
-			for(int j=0; j<=arr[i].length; j++)
+			for(int i=0; i<=arr.length; i++)
 			{
-				arr[i][j] = count++;				
+				for(int j=0; j<=arr[i].length; j++)
+				{
+					arr[i][j] = count++;				
+				}
+			}
+			for(int i=0; i<=arr.length; i++)
+			{
+				for(int j=0; j<=arr[i].length; j++)
+				{
+					System.out.println(arr[i][j]);
+				}
 			}
 		}
-		for(int i=0; i<=arr.length; i++)
+		catch (Exception e) 
 		{
-			for(int j=0; j<=arr[i].length; j++)
-			{
-				System.out.println(arr[i][j]);
-			}
+			System.out.println(e);
 		}
+		
 	}
 }
