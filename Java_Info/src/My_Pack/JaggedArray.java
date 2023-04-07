570cbe4ca8a08e6d602c0db24bfc3981505baf15
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-07 12:54:44 +0530

    Add...

--- Java_Info/src/My_Pack/JaggedArray.java
+++ Java_Info/src/My_Pack/JaggedArray.java
@@ -11,7 +11,10 @@ public class JaggedArray
 		int count = 0;
 		for(int i=0; i<=arr.length; i++)
 		{
-			
+			for(int j=0; j<=arr[i].length; j++)
+			{
+				arr[i][j] = count++;				
+			}
 		}
 		
 	}
