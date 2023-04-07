1a208bb1644bae922f58e8141ed144a385122e2d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-07 11:21:59 +0530

    Add...

--- Java_Info/src/My_Pack/FileHandlingDemo.java
+++ Java_Info/src/My_Pack/FileHandlingDemo.java
@@ -4,13 +4,14 @@ import java.io.*;
 
 public class FileHandlingDemo {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
 		FileInputStream in = null;
 		FileOutputStream out = null;
 		try
 		{
-			in = new FileInputStream("input.txt");
+			System.out.println("Program Start...");
+			in = new FileInputStream("/home/vishal/Desktop/input.txt");
 			out = new FileOutputStream("Output.txt");
 			int c;
 			while((c = in.read()) != -1)
@@ -20,12 +21,21 @@ public class FileHandlingDemo {
 		}
 		catch (Exception e) 
 		{
-		
+			System.out.println(e);
 		}
 		finally
 		{
+			if(in!=null)
+			{
+				in.close();
+			}
 			
+			if(out != null)
+			{
+				out.close();
+			}
 		}
+		
 	}
 
 }
