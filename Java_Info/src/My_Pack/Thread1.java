7b6c576a22b912e614936ae98d86fa68ef674d3f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-17 21:24:40 +0530

    Add...

--- Java_Info/src/My_Pack/Thread1.java
+++ Java_Info/src/My_Pack/Thread1.java
@@ -10,9 +10,10 @@ public class Thread1 extends Thread
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		Thread1 t = new Thread1();
-		t.start();
+		Thread1 t1= new Thread1();
+		Thread1 t2= new Thread1();
+		t1.start();
+		t2.start();
 		System.out.println("Thread...");
 	}
 }
