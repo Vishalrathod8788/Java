8e08a499c5049846efb18199833cabbc11bb1b14
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-18 18:17:39 +0530

    Add...

--- Java_Info/src/My_Pack/OneDArrayWithException.java
+++ Java_Info/src/My_Pack/OneDArrayWithException.java
@@ -5,13 +5,18 @@ public class OneDArrayWithException extends Thread
 	
 	public static void main(String[] args) 
 	{
-		OneDArrayWithException t1 = new OneDArrayWithException();
-		int num[] = {10,20,30,40,50};
-			for(int i=0; i<=4; i++)
-			{
-				System.out.println(num[i]);
-				
-			}
+		try {
+			OneDArrayWithException t1 = new OneDArrayWithException();
+			int num[] = {10,20,30,40,50};
+				for(int i=0; i<=4; i++)
+				{
+					System.out.println(num[i]);
+					
+				}	
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
 }
