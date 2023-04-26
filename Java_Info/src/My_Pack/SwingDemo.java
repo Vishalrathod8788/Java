2e77b51735bf52523dfeb0d4878439003cc909b8
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-04-26 19:03:28 +0530

    Add...

--- Java_Info/src/My_Pack/SwingDemo.java
+++ Java_Info/src/My_Pack/SwingDemo.java
@@ -2,11 +2,14 @@ package My_Pack;
 
 import javax.swing.*;
 
-public class SwingDemo {
+public class SwingDemo extends JFrame
+{
 
+	
 	public static void main(String[] args) 
 	{
-
+		JLabel lb = new JLabel();
+		
 	}
 
 }
